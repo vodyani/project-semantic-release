7586c39d0ece56a897229fcc62a18c3edfb227b4
John Denver <chogathkk@gmail.com>
2022-08-26 14:41:48 +0800

    feat: first commit 🌈

---
+++ .releaserc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "branches": [
+    "master",
+    "next",
+    "next-major",
+    "+([0-9])?(.{+([0-9]),x}).x",
+    {
+      "name": "beta",
+      "prerelease": true
+    },
+    {
+      "name": "alpha",
+      "prerelease": true
+    },
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "package-lock.json",
+          "package.json",
+          "CHANGELOG.md"
+        ],
+        "message": "chore(🤖): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github"
+  ]
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# project-semantic-release
+# semantic-release-config
+
+用于发布和管理 vodyani 项目的 release 版本。
